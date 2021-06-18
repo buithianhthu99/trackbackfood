80e9489e624a6a28c773e695b1d8aad56a98b1ca
buithianhthu99 <buithianhthu99>
2021-06-19 01:34:18 +0700

    implement smart contract

--- clientapp/src/myblockchainAbi.js
+++ clientapp/src/myblockchainAbi.js
@@ -1,38 +1,204 @@
 export const myblockchainAbi = [
-    {
-      inputs: [],
-      payable: false,
-      stateMutability: "nonpayable",
-      type: "constructor"
-    },
-    {
-      constant: true,
-      inputs: [],
-      name: "getGreeting",
-      outputs: [
-        {
-          internalType: "string",
-          name: "",
-          type: "string"
-        }
-      ],
-      payable: false,
-      stateMutability: "view",
-      type: "function"
-    },
-    {
-      constant: false,
-      inputs: [
-        {
-          internalType: "string",
-          name: "_greeting",
-          type: "string"
-        }
-      ],
-      name: "setGreeting",
-      outputs: [],
-      payable: false,
-      stateMutability: "nonpayable",
-      type: "function"
-    }
-  ];
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getGreeting",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_greeting",
+        "type": "string"
+      }
+    ],
+    "name": "setGreeting",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "harvestId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "productId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_ingredients",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_startTime",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_endTime",
+        "type": "string"
+      }
+    ],
+    "name": "addProcess",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "harvestId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      }
+    ],
+    "name": "addProduct",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "harvestId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "productId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_amount",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_state",
+        "type": "string"
+      }
+    ],
+    "name": "updateProduct",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "address",
+        "name": "_owner",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "_startTime",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_endTime",
+        "type": "string"
+      }
+    ],
+    "name": "addHarvest",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "id",
+        "type": "uint256"
+      }
+    ],
+    "name": "getHarvest",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
--- contracts/myblockchain.sol
+++ contracts/myblockchain.sol
@@ -2,9 +2,39 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract myblockchain {
     string greeting;
+    mapping(uint => Harvest) harvests;
+    uint harvestsAmount;
+
+    struct Harvest  {
+        uint harvestId;
+        string name;
+        mapping(uint => Product) products;
+        uint productsAmount;
+        address owner;
+        string startTime;
+        string endTime;
+    }
+
+    struct Product {
+        uint productId;
+        string name;
+        mapping(uint => Process) processes;
+        uint processesAmount;
+        uint amount;
+        string state;
+    }
+
+    struct Process {
+        uint processId;
+        string name;
+        string ingredients;
+        string startTime;
+        string endTime;
+    }
 
     constructor() public {
         greeting = "hello";
+        harvestsAmount = 0;
     }
 
     function getGreeting() public view returns(string memory) {
@@ -13,5 +43,36 @@ contract myblockchain {
 
     function setGreeting(string memory _greeting) public {
         greeting = _greeting;
+    }
+
+    function addProcess(uint harvestId, uint productId, string memory _name, string memory _ingredients, string memory _startTime, string memory _endTime) public {
+        uint id = harvests[harvestId].products[productId].processesAmount;
+        Product storage product = harvests[harvestId].products[productId];
+        product.processes[id] = Process(id, _name, _ingredients, _startTime, _endTime);
+        product.processesAmount++;
+    }
+
+    function addProduct(uint harvestId, string memory _name) public {
+        uint id = harvests[harvestId].productsAmount;
+        Harvest storage harvest = harvests[harvestId];
+        harvest.products[id] = Product(id, _name, 0, 0, "unharvested");
+        harvest.productsAmount++;
+    }
+
+    function updateProduct(uint harvestId, uint productId, string memory _name, uint _amount, string memory _state) public {
+        Product storage product = harvests[harvestId].products[productId];
+        product.name = _name;
+        product.amount = _amount;
+        product.state = _state;
+    }
+
+    function addHarvest(string memory _name, address _owner, string memory _startTime, string memory _endTime) public {
+        uint id = harvestsAmount;
+        harvests[id] = Harvest(id, _name, 0, _owner, _startTime, _endTime);
+        harvestsAmount++;
+    }
+
+    function getHarvest(uint id) public view returns(string memory, uint, address, string memory, string memory) {
+        return (harvests[id].name, harvests[id].productsAmount, harvests[id].owner, harvests[id].startTime, harvests[id].endTime);
     }
 }
