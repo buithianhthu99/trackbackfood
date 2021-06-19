f4c01efd72415e548a12412a02d95b8c87f160af
Pham Ngoc Son <43614413+pns1681999@users.noreply.github.com>
2021-06-19 15:37:24 +0700

    main layout

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -1,107 +1,185 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
+  <q-layout view="lHh lpR lFf" class="bg-grey-1">
+    <q-header bordered class="bg-white text-grey-8">
+      <q-toolbar class="G4U__toolbar">
+        <q-btn flat dense round @click="leftDrawerOpen = !leftDrawerOpen" aria-label="Menu" icon="menu" class="q-mr-sm" />
 
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title shrink class="row items-center no-wrap">
+            <div>Trackback</div>
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space />
+
+        <div class="q-gutter-sm row items-center no-wrap">
+
+          <q-btn flat no-caps v-if="account">
+            <q-avatar round text-color="white" color="primary">
+              <span>{{ account | avatar }}</span>
+            </q-avatar>
+            <span class="q-pl-xs" v-if="$q.screen.gt.xs">{{ account }}</span>
+
+            <q-menu transition-show="scale" transition-hide="scale" fit>
+              <q-list style="min-width: 200px">
+                <q-item clickable to="/account">
+                  <q-item-section side>
+                    <q-icon name="manage_accounts" />
+                  </q-item-section>
+                  <q-item-section>Manage Account</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable @click="signOut">
+                  <q-item-section side>
+                    <q-icon name="logout" />
+                  </q-item-section>
+                  <q-item-section>Sign out</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-white column" :width="280">
+      <q-scroll-area class="G4U__drawer-menu">
+        <q-list padding class="text-grey-8">
+          <q-item
+            class="G4U__drawer-item"
+            v-ripple
+            v-for="link in links1"
+            :key="link.text"
+            clickable
+            :to="link.to"
+            active-class="bg-teal-1 text-grey-8"
+          >
+            <q-item-section avatar>
+              <q-icon :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator inset class="q-my-sm" />
+          <q-item
+            class="G4U__drawer-item"
+            v-ripple
+            v-for="link in links5"
+            :key="link.text"
+            clickable
+            :to="link.to"
+            active-class="bg-teal-1 text-grey-8"
+          >
+            <q-item-section avatar>
+              <q-icon :name="link.icon" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>{{ link.text }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+      <div>
+        <q-item>
+          <q-item-section>
+            <q-item-label>Trackback Food Blockchain System</q-item-label>
+            <q-item-label caption lines="2">All rights reserved</q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container class="G4U__content">
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 export default {
-  name: 'MainLayout',
-  components: { EssentialLink },
-  data () {
+  name: 'G4ULayout',
+  data() {
     return {
+      slide: 0,
+      showThumnail: false,
+      slides: [],
+
+      year: new Date().getFullYear(),
       leftDrawerOpen: false,
-      essentialLinks: linksData
+      links1: [
+        { icon: 'dashboard', text: 'Harvest', to: '/harvest' },
+      ],
+      links5: [{ icon: 'settings', text: 'Settings', to: '/settings' }],
+    };
+  },
+  filters: {
+    avatar: function (value = '') {
+      return value.substr(0, 2).toUpperCase();
+    },
+  },
+  computed: {
+    account() {
+      return this.$q.localStorage.getItem('account')
+    }
+  },
+  methods: {
+    signOut() {
+      
+    },
+
+  },
+
+  created() {
+    console.log(this.account)
+  },
+
+};
+</script>
+
+<style lang="scss">
+.G4U {
+  &__content {
+    // max-width: 1600px;
+    margin: 0 auto;
+  }
+
+  &__toolbar-input {
+    width: 55%;
+  }
+  &__drawer-menu {
+    width: 100%;
+    flex: 1;
+    // height: calc(100% - 64px);
+  }
+  &__drawer-item {
+    line-height: 24px;
+    border-radius: 0 24px 24px 0;
+    margin-right: 12px;
+    .q-item__section--avatar {
+      .q-icon {
+        color: #5f6368;
+      }
+    }
+
+    .q-item__label {
+      color: #3c4043;
+      letter-spacing: 0.01785714em;
+      font-size: 0.875rem;
+      font-weight: 500;
+      line-height: 1.25rem;
+    }
+  }
+
+  &__drawer-footer-link {
+    color: inherit;
+    text-decoration: none;
+    font-weight: 500;
+    font-size: 0.75rem;
+
+    &:hover {
+      color: #000;
     }
   }
 }
-</script>
+</style>
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,6 +5,27 @@ const routes = [
     children: [{ path: "", component: () => import("pages/Index.vue") }]
   },
   {
+    path: '/harvest',
+    component: () => import('pages/harvest/Index.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'harvest',
+        component: () => import('pages/harvest/List.vue'),
+      },
+      {
+        path: '/harvest/new',
+        name: 'harvest-new',
+        component: () => import('pages/harvest/New.vue'),
+      },
+      {
+        path: '/harvest/:id',
+        name: 'harvest-id',
+        component: () => import('pages/harvest/_Id.vue'),
+      },
+    ],
+  },
+  {
     path: "/Signin",
     component: () => import("layouts/LoginLayout.vue"),
     children: [{ path: "", component: () => import("src/pages/Signin.vue") }]
