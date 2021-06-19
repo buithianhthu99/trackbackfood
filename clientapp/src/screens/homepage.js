ea648870b11fd9fbf2fd5c8eeee1b3d58120b902
buithianhthu99 <buithianhthu99>
2021-06-19 09:34:38 +0700

    Update homepage.js

--- clientapp/src/screens/homepage.js
+++ clientapp/src/screens/homepage.js
@@ -1,56 +1,56 @@
 import React, { useState } from "react";
-import Menu from '../components/menu.js'
-import Web3 from 'web3';
-import {myblockchainAbi} from '../myblockchainAbi'
-import { contract_address } from "../config.js";
+import Menu from '../components/menu.js';
+import { Button } from '@material-ui/core';
 
 export default function Homepage() {
-  const currentProvider = new Web3.providers.HttpProvider('http://localhost:8545');
-  const web3 = new Web3(currentProvider)
-
-  const contractAddress = contract_address;
-  const myblockchainContract = new web3.eth.Contract(myblockchainAbi, contractAddress);
-
   const account = localStorage.getItem("account");
-  const [greeting, setGreeting] = useState(localStorage.getItem("account"));
   
-  const setData = async e => {
-    e.preventDefault();    
-    const gas = await myblockchainContract.methods.setGreeting(greeting).estimateGas();
-    const result = await myblockchainContract.methods
-      .setGreeting(greeting)
-      .send({ from: account, gas });
-    console.log(result);
-  };
+  // //Add new harvest
+  // const data = {name: "test_harvest", owner: localStorage.getItem("account"), startTime: "10/06/2021", endTime: "20/06/2021"}
+  // const handleSubmit = () => {      
+  //   console.log('Post method');
+  //   fetch("http://localhost:3000/harvests/", {            
+  //       method: 'post',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(data)
+  //   })
 
-  const getData = async e => {
-    e.preventDefault();
-    const result = await myblockchainContract.methods.getGreeting().call();
-    setGreeting(result);
-    console.log(result);
-  };
+  // //Add new product
+  // const data = {harvestId: 0, owner: localStorage.getItem("account"), name: "Rice"}
+  // const handleSubmit = () => {      
+  //   console.log('Post method');
+  //   fetch("http://localhost:3000/products/", {            
+  //       method: 'post',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(data)
+  //   })
+
+  // //Update product info
+  // const data = {harvestId: 0, productId: 0, name: "Name update", amount: 10, state: "harvested", owner: localStorage.getItem("account")}
+  // const handleSubmit = () => {      
+  //   console.log('Post method');
+  //   fetch("http://localhost:3000/products/update/", {            
+  //       method: 'post',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(data)
+  //   })
+
+  //Add process
+  // const data = {harvestId: 0, productId: 0, name: "Test process", ingredients: "Saturated Fat 5g, Dietary Fiber 2g", startTime: "10/06/2021", endTime: "10/06/2021", owner: localStorage.getItem("account")}
+  // const handleSubmit = () => {      
+  //   console.log('Post method');
+  //   fetch("http://localhost:3000/processes/", {            
+  //       method: 'post',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(data)
+  //   })
+}
 
   return (
     <div className="App">
         <Menu></Menu>
-        <header className="App-header">
-            <form onSubmit={setData}>
-                <label>
-                    Set Data:
-                    <input
-                    type="text"
-                    name="greeting"
-                    value={greeting}
-                    onChange={e => setGreeting(e.target.value)}
-                    />
-                </label>
-          <input type="submit" value="Set Data" />
-        </form>
-        <br />
-        <button onClick={getData} type="button">
-            Get Data
-        </button>
-      </header>
+        <p>{account}</p>
+        <Button onClick={handleSubmit}>Click</Button>
     </div>
   );
 }
